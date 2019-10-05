dbfa2cd402b70a266e63203c615de0c4448f458c
szenadam <szenadam1@gmail.com>
2019-10-05 17:19:03 +0200

    Align everything to center with flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Dog from "./dog.svg";
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <h1>Fetch me a Dog!</h1>
       <img src={Dog} alt="Dog logo" className="dog-logo" />
       <button className="btn">Fetch!</button>
@@ -13,7 +13,7 @@ function App() {
         alt="A cute dog"
         className="dog-image"
       />
-    </>
+    </div>
   );
 }
 
