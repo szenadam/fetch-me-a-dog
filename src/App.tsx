a072e274f70193a791dd028f59ee430f0ed6ae47
szenadam <szenadam1@gmail.com>
2020-01-07 21:10:13 +0100

    Create GithubBadge comp, use it so fill color can be changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Dog from './assets/dog.svg';
-import GithubCornerBadge from './assets/github-corner-right.svg';
 
 interface DogApiResponse {
   message: string;
   status: string;
 }
 
-interface Theme {
-  btn: string;
+interface GithubBadgeProps {
+  backgroundFill: string;
+  octoFill: string;
+}
+
+function GithubBadge(props: GithubBadgeProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      viewBox="0 0 250 250"
+      fill={props.backgroundFill}
+      className='github-badge-right'
+      style={{position: 'absolute', top: 0, right: 0}}
+    >
+      <path d="M0 0l115 115h15l12 27 108 108V0z" fill={props.octoFill} />
+      <path
+        className="octo-arm"
+        d="M128 109c-15-9-9-19-9-19 3-7 2-11 2-11-1-7 3-2 3-2 4 5 2 11 2 11-3 10 5 15 9 16"
+        style={{WebkitTransformOrigin: '130px 106px', transformOrigin: "130px 106px"}}
+      />
+      <path
+        className="octo-body"
+        d="M115 115s4 2 5 0l14-14c3-2 6-3 8-3-8-11-15-24 2-41 5-5 10-7 16-7 1-2 3-7 12-11 0 0 5 3 7 16 4 2 8 5 12 9s7 8 9 12c14 3 17 7 17 7-4 8-9 11-11 11 0 6-2 11-7 16-16 16-30 10-41 2 0 3-1 7-5 11l-12 11c-1 1 1 5 1 5z"
+      />
+    </svg>
+  );
 }
 
 function App() {
@@ -56,7 +81,7 @@ function App() {
   return (
     <div className="wrapper">
       <a href="https://github.com/szenadam/fetch-me-a-dog" target="_blank" rel="noreferrer noopener">
-        <img className="github-badge-right" src={GithubCornerBadge} alt="GitHub repository page" />
+        <GithubBadge backgroundFill='#f9f6e7' octoFill='#39566e' />
       </a>
       <h1>Fetch me a Dog!</h1>
       <div className="color-palette">
